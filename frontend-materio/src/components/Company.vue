85a5c7bd3e8edf209d4c02f13f94f09a890ba459
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-12-18 10:06:24 +0900

    commit

---
+++ frontend-materio/src/components/Company.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <String label="Name" v-model="value.name" :editMode="editMode"/>
+        <String label="Industry" v-model="value.industry" :editMode="editMode"/>
+        <Date label="FoundedDate" v-model="value.foundedDate" :editMode="editMode"/>
+        <Status offline label="ProductStatus" v-model="value.productStatus" :editMode="editMode" @change="change"/>
+
+        <v-divider class="border-opacity-100 my-divider"></v-divider>
+        <v-layout row justify-end>
+            <v-btn
+                width="64px"
+                color="primary"
+                @click="save"
+            >
+                저장
+            </v-btn>
+        </v-layout>
+    </div>
+</template>
+
+
+<script>
+
+import BaseEntity from './base-ui/BaseEntity.vue'
+import String from './primitives/String.vue'
+import Date from './primitives/Date.vue'
+import Status from './Status.vue'
+
+export default {
+    name: 'Company',
+    mixins:[BaseEntity],
+    components:{
+        String,
+        Date,
+        Status,
+    },
+    
+    data: () => ({
+        path: "companies"
+    }),
+    created(){
+    },
+    computed:{
+    },
+    methods: {
+    },
+}
+</script>
---
+++ frontend-materio/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+        <VerticalNavSectionTitle :item="{ heading: '기준정보' }" />
+        <VerticalNavLink
+            :item="{
+                title: '회사정보',
+                to: '/companies',
+            }"
+        />
+        <VerticalNavLink
+            :item="{
+                title: '상품 정보',
+                to: '/products',
+            }"
+        />
+        <VerticalNavSectionTitle :item="{ heading: '수주정보' }" />
+        <VerticalNavLink
+            :item="{
+                title: '수주정보',
+                to: '/salesOrders',
+            }"
+        />
+        <VerticalNavSectionTitle :item="{ heading: '재고정보' }" />
+        <VerticalNavLink
+            :item="{
+                title: '재고',
+                to: '/inventories',
+            }"
+        />
+    </ul>
+</template>
